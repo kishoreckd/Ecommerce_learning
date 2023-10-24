7b0ae9e7e6a83a04d4a548610340b6b7d1cf2af2
Kishore Kumar <kishorekumarcdckap@gmail.com>
2023-10-24 10:17:05 +0530

    page builder used

--- lib/src/routing/app_router.dart
+++ lib/src/routing/app_router.dart
@@ -1,3 +1,4 @@
+import 'package:flutter/material.dart';
 import 'package:go_router/go_router.dart';
 import 'package:ecommerce_app/src/features/shopping_cart/shopping_cart_screen.dart';
 import 'package:ecommerce_app/src/features/products_list/products_list_screen.dart';
@@ -14,7 +15,12 @@ final gorouter = GoRouter(
         GoRoute(
           path: 'cart',
           builder: (context, state) => const ShoppingCartScreen(),
+           pageBuilder: (context, state) => MaterialPage(
+            key: state.pageKey,
+            fullscreenDialog: true,
+            child:ShoppingCartScreen() 
         ),
+        )
       ],
     ),
   ],

c9e4de2cde2e993619a4f3791ad07e32d179f3df
Kishore Kumar <kishorekumarcdckap@gmail.com>
2023-10-25 10:27:27 +0530

    Fake product repository Future and stream has been added

--- lib/src/features/products/data/fake_product_repository.dart
+++ lib/src/features/products/data/fake_product_repository.dart
@@ -2,15 +2,32 @@ import 'package:ecommerce_app/src/constants/test_products.dart';
 import 'package:ecommerce_app/src/features/products/domain/product.dart';
 
 class FakeProductRepository {
-  /**This below line will create the private constructor */
+  final List<Product> _products = kTestProducts;
+
+  /// This below line will create the private constructor
   FakeProductRepository._();
   static FakeProductRepository instance = FakeProductRepository._();
 
   List<Product> getProductList() {
-    return kTestProducts;
+    return _products;
   }
 
   Product? getProduct(String id) {
-    return kTestProducts.firstWhere((product) => product.id == id);
+    return _products.firstWhere((product) => product.id == id);
+  }
+
+  /// Future will fetch from APIs
+  Future<List<Product>> fetchProductsList() {
+    return Future.value(_products);
+  }
+
+  /// Stream will watch all the APIs
+  Stream<List<Product>> watchProductList() {
+    return Stream.value(_products);
+  }
+
+  Stream<Product?> watchProduct(String id) {
+    return watchProductList()
+        .map((products) => products.firstWhere((product) => product.id == id));
   }
 }

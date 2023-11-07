d9542ebd4331bb6f6779b576063fbf7239eb0e3e
Kishore Kumar <kishorekumarcdckap@gmail.com>
2023-11-07 11:22:52 +0530

    every unit test has been done , group test

--- test/src/features/products/data/fake_products_repository_test.dart
+++ test/src/features/products/data/fake_products_repository_test.dart
@@ -3,8 +3,44 @@ import 'package:ecommerce_app/src/constants/test_products.dart';
 import 'package:ecommerce_app/src/features/products/data/fake_products_repository.dart';
 
 void main() {
-  test('getProductsList returns global list', () {
-    final productsRepository = FakeProductsRepository();
-    expect(productsRepository.getProductsList(), kTestProducts);
+  group('FakeProductRepository', () {
+    test('getProductsList returns global list', () {
+      final productsRepository = FakeProductsRepository();
+      expect(productsRepository.getProductsList(), kTestProducts);
+    });
+
+    test('getProduct(1) returns first item', () {
+      final productRepository = FakeProductsRepository();
+      expect(productRepository.getProduct('1'), kTestProducts[0]);
+    });
+
+    test('getProduct(100) returns null', () {
+      final productRepository = FakeProductsRepository();
+      expect(productRepository.getProduct('100'), null);
+    });
+  });
+
+  test('fetchProductList rteurns global list', () async {
+    final productRepository = FakeProductsRepository();
+
+    expect(await productRepository.fetchProductsList(), kTestProducts);
+  });
+
+  test('watchProductsList emits global list', () {
+    final productRepository = FakeProductsRepository();
+
+    expect(productRepository.watchProductsList(), emits(kTestProducts));
+  });
+
+  test('watchProduct(1) emits first item', () {
+    final productRepository = FakeProductsRepository();
+
+    expect(productRepository.watchProduct('1'), emits(kTestProducts[0]));
+  });
+
+  test('watchProduct(100) emits null', () {
+    final productRepository = FakeProductsRepository();
+
+    expect(productRepository.watchProduct('100'), emits(null));
   });
 }
